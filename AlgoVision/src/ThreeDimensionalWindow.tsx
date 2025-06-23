f3dd397e2dea1f9e21d2fbeab882549b18a55713
nsardinia <nicholas_sardinia@outlook.com>
2025-06-22 22:01:55 -0400

    added points and lines to the 3d window. Graph generation -> soon

--- AlgoVision/src/ThreeDimensionalWindow.tsx
+++ AlgoVision/src/ThreeDimensionalWindow.tsx
@@ -12,12 +12,37 @@ function ThreeDimensionalWindow() {
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
             <OrbitControls />
-            <mesh>
+            {/* <mesh position={[1, 1, 1]}>
                 <boxGeometry args={[2, 2, 2]}/>
                 <meshStandardMaterial />
             </mesh>
-        <gridHelper />
-        <axesHelper args={[5]} />
+            <mesh position={[0, 0, 0]}>
+                <sphereGeometry args={[1, 14, 14]}/>
+                <meshStandardMaterial color="hotpink"/>
+            </mesh> */}
+            <line>
+              <bufferGeometry>
+                <bufferAttribute
+                  attach="attributes-position"
+                  args={[new Float32Array([0, 0, 1, 0, 0, 2, 1, 2, 3]), 3]} 
+                />
+
+              </bufferGeometry>
+              <lineBasicMaterial color="white" linewidth={2} />
+            </line>
+            <points>
+              <bufferGeometry>
+              <bufferAttribute
+                attach="attributes-position"
+                args={[new Float32Array([0, 0, 1, 0, 0, 2, 1, 2, 3]), 3]}
+              />
+              </bufferGeometry>
+              <pointsMaterial color="hotpink" size={0.1} sizeAttenuation />  
+            </points>
+
+
+        {/* <gridHelper /> */}
+        {/* #<axesHelper args={[5]} /> */}
         </Canvas>
         </div>
   )
