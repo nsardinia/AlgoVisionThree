4e214a69b88c949e308da1d576f0b46b9f52ab7c
root <root@LAPTOP-8LL061ND.localdomain>
2025-07-20 19:51:47 -0400

    added backend starter

---
+++ backend/src/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+"fmt"
+"net/http"
+"log"
+)
+
+func helloHandler(w http.ResponseWriter, r *httpRequest) {
+w.Write([]byte("msg received\r\n"))
+fmt.Println("received req from client")
+}
+
+func helloHandlerPost(w http.ResponseWriter, r *http.Request) {
+w.Write([]byte("msmg.received\r\n")
+fmt.Println("received POSTS req from client")
+}
+
+func main() {
+fmt.Println("Hello, Server!")
+
+mux := http.NewServeMux()
+hf := http.HandlerFunc(helloHandler)
+hfp := http.HandlerFunc(helloHandlerPost)
+mux.Handle("GET /", hf)
+mux.Handle("POST /", hfp)
+
+server := http.Server{Addr: ":8080", Handler: mux}
+nerr := server.ListenAndServe()
+if nerr != nil {
+	log.Fatalf("Server failed to start: %v", nerr)
+}
+}
